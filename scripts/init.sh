1f9710bc1e1b30462205bcacb6744cdd615c99b2
shira <shirass321@gmail.com>
2023-09-27 21:12:50 +0300

    added style to chat-app

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,13 +1,18 @@
+# #!/bin/bash
+
+# read -p "enter the version to run: "  version
+
+# appname="chat-app"
+# image_name="${appname}:${version}"
+
+# docker volume create chat-app-data
+# artifact_registry_image=me-west1-docker.pkg.dev/grunitech-mid-project/shira-shani-chat-app-images/${appname}:${version}
+# gcloud compute ssh shira-shani-first-instance --project grunitech-mid-project --zone me-west1-a 
+# gcloud auth configure-docker me-west1-docker.pkg.dev
+# docker pull ${artifact_registry_image}
+# docker run -p 8080:5000 ${artifact_registry_image}
 #!/bin/bash
-
-read -p "enter the version to run: "  version
-
-appname="chat-app"
-image_name="${appname}:${version}"
-
 docker volume create chat-app-data
-artifact_registry_image=me-west1-docker.pkg.dev/grunitech-mid-project/shira-shani-chat-app-images/${appname}:${version}
-gcloud compute ssh shira-shani-first-instance --project grunitech-mid-project --zone me-west1-a 
-gcloud auth configure-docker me-west1-docker.pkg.dev
-docker pull ${artifact_registry_image}
-docker run -p 8080:5000 ${artifact_registry_image}
+docker build -t  chat-app .
+# docker run --name chat-run -p 5000:5000 chat-app 
+docker run -v chat-app-data:/chatApp/data -p 5001:5000 chat-app
