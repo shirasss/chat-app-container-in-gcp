32f9669f9b751bd57014a91826c58c921eaa7233
shira <shirass321@gmail.com>
2023-09-28 18:48:18 +0300

    p

--- scripts/init.sh
+++ scripts/init.sh
@@ -12,7 +12,7 @@ image_name="${appname}:${version}"
 # docker run -p 8080:5000 ${artifact_registry_image}
 
 artifact_registry_image=me-west1-docker.pkg.dev/grunitech-mid-project/shira-shani-chat-app-images/${appname}:${version}
-gcloud compute ssh shira-shani-first-instance --project grunitech-mid-project --zone me-west1-a  --command "gcloud auth configure-docker me-west1-docker.pkg.dev; docker volume create chat-app-data; docker pull ${artifact_registry_image}; docker run -p 8080:5000 ${artifact_registry_image}"
+gcloud compute ssh shira-shani-first-instance --project grunitech-mid-project --zone me-west1-a  --command "gcloud auth configure-docker me-west1-docker.pkg.dev; docker volume create chat-app-data; docker pull ${artifact_registry_image}; docker run -p 81:5000 ${artifact_registry_image}"
 
 #!/bin/bash
 # docker volume create chat-app-data
